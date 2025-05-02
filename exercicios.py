f3f09e73d591cbdde1578d529bdb423b0dd384d0
Brenno Vieira <brennovieira16.95@gmail.com>
2025-05-02 17:59:05 -0300

    atualizando o exercicio 01

--- exercicios.py
+++ exercicios.py
@@ -1,6 +1,9 @@
-valor_1 = int(input("inserir um numero inteiro:"))
-valor_2 = int(input("inserir outro numero inteiro:"))
-
-resultado = valor_1 // valor_2
-
-print(resultado)
+try:
+    valor_1 = int(input("inserir um numero inteiro:"))
+    valor_2 = int(input("inserir outro numero inteiro:"))
+    resultado = valor_1 // valor_2
+    print(resultado)
+except ZeroDivisionError:
+    print("integer division or modulo by zero ")
+except KeyboardInterrupt:
+    print("acho que você nao quis inserir um numero")
