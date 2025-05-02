0b40246925cb70668ea70fd2df02aed49baef6bb
Brenno Vieira <brennovieira16.95@gmail.com>
2025-05-02 17:38:58 -0300

    adicionando o exercicio 14

---
+++ exercicio14.py
@@ -0,0 +1,6 @@
+input("Insira uma data no formado dd/mm/aaaa: ")
+data_do_usuario = "01/05/2025"
+lista_de_dia_mes_ano = data_do_usuario.split("/")
+print(f"O elemento 1 e o: {lista_de_dia_mes_ano[0]}")
+print(f"O elemento 1 e o: {lista_de_dia_mes_ano[1]}")
+print(f"O elemento 1 e o: {lista_de_dia_mes_ano[2]}")
